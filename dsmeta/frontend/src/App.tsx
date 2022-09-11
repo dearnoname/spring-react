97b7751b9fa19b4864fb31adee12b0550b38bbaf
Jose Castillo <josecastillococ@gmail.com>
2022-09-11 13:02:56 -0300

    Layout atualizado

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SaleCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-    <h1>Olá Mundo</h1>
-    <NotificationButton />
+    <Header/>
+    <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SaleCard />
+        </div>
+      </section>
+    </main>
     </>
   )
  
